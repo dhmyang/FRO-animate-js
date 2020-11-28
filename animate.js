67faf178b540cf5b23951ce2277a4ea30b4f1c42
dhmyang <dhm.yang@hotmail.com>
2020-11-28 20:05:38 +0100

    Vid 6 push (Error)
    Animation for navbar die niet werkt na een uur, dus kgeef het op

--- animate.js
+++ animate.js
@@ -1,3 +1,18 @@
 document.querySelector('.animeer-knop').addEventListener('click', (e)=>{
-    document.querySelector('.vlakje').classList.toggle('box_right')
-})
+    document.querySelector('.vlakje').classList.toggle('box_right');
+});
+
+setTimeout( () => {
+    document.querySelector('.vlakje').classList.toggle('box_right');
+}, 2000);
+
+
+const menuBtn = document.querySelector('.nav__btn');
+const navItem = document.querySelector('nav__link');
+menuBtn.addEventListener('click', () => {
+    navItem.forEach( (item, index) =>{
+        setTimeout(() => {
+            item.classList.toggle('nav__link--slide');
+        }, 100 * index);
+    })
+});
