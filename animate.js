baef1cf05fe0b419e23b4991e2b3c24c67cfca37
dhmyang <55231982+dhmyang@users.noreply.github.com>
2020-12-08 01:29:58 +0100

    vid6 gefixt

--- animate.js
+++ animate.js
@@ -1,18 +1,17 @@
-document.querySelector('.animeer-knop').addEventListener('click', (e)=>{
-    document.querySelector('.vlakje').classList.toggle('box_right');
+document.querySelector(".animeer-knop").addEventListener("click", (e) => {
+  document.querySelector(".vlakje").classList.toggle("box_right");
 });
 
-setTimeout( () => {
-    document.querySelector('.vlakje').classList.toggle('box_right');
+setTimeout(() => {
+  document.querySelector(".vlakje").classList.toggle("box_right");
 }, 2000);
 
-
-const menuBtn = document.querySelector('.nav__btn');
-const navItem = document.querySelector('nav__link');
-menuBtn.addEventListener('click', () => {
-    navItem.forEach( (item, index) =>{
-        setTimeout(() => {
-            item.classList.toggle('nav__link--slide');
-        }, 100 * index);
-    })
+const menuBtn = document.querySelector(".nav__btn");
+const navItem = document.querySelectorAll(".nav__link");
+menuBtn.addEventListener("click", () => {
+  navItem.forEach((item, index) => {
+    setTimeout(() => {
+      item.classList.toggle("nav__link--slide");
+    }, 100 * index);
+  });
 });
